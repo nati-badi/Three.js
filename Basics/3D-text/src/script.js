43d3002eb20a9722b2777c6529c94703237ac38d
Nati <natibadigojimma@gmail.com>
2024-11-30 16:13:39 -0800

    trying different matcap textures

--- Basics/3D-text/src/script.js
+++ Basics/3D-text/src/script.js
@@ -24,7 +24,7 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("textures/matcaps/8.png");
+const matcapTexture = textureLoader.load("textures/matcaps/7.png");
 
 /**
  * Fonts
