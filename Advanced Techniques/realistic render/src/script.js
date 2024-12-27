bfca5ed219ddcfe1ef6c91820e3941094047a253
Nati <natibadigojimma@gmail.com>
2024-12-27 05:26:42 -0800

    optimization(realistic render): applying shadows to my hamburger model i made with blender

--- Advanced Techniques/realistic render/src/script.js
+++ Advanced Techniques/realistic render/src/script.js
@@ -34,6 +34,8 @@ const updateAllMaterials = () => {
     ) {
       // child.material.envMap = environmentMap;
       child.material.envMapIntensity = debugObject.envMapIntensity;
+      child.receiveShadow = true;
+      child.castShadow = true;
     }
   });
 };
@@ -42,12 +44,12 @@ const updateAllMaterials = () => {
  * Environment map
  */
 const environmentMap = cubeTextureLoader.load([
-  "/textures/environmentMaps/0/px.jpg",
-  "/textures/environmentMaps/0/nx.jpg",
-  "/textures/environmentMaps/0/py.jpg",
-  "/textures/environmentMaps/0/ny.jpg",
-  "/textures/environmentMaps/0/pz.jpg",
-  "/textures/environmentMaps/0/nz.jpg",
+  "/textures/environmentMaps/3/px.jpg",
+  "/textures/environmentMaps/3/nx.jpg",
+  "/textures/environmentMaps/3/py.jpg",
+  "/textures/environmentMaps/3/ny.jpg",
+  "/textures/environmentMaps/3/pz.jpg",
+  "/textures/environmentMaps/3/nz.jpg",
 ]);
 environmentMap.encoding = THREE.sRGBEncoding;
 scene.background = environmentMap;
@@ -64,9 +66,9 @@ gui
 /**
  * Models
  */
-gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
-  gltf.scene.scale.set(10, 10, 10);
-  gltf.scene.position.set(0, -4, 0);
+gltfLoader.load("/models/hamburger.glb", (gltf) => {
+  gltf.scene.scale.set(0.3, 0.3, 0.3);
+  gltf.scene.position.set(0, -1, 0);
   gltf.scene.rotation.y = Math.PI * 0.5;
   scene.add(gltf.scene);
 
@@ -85,7 +87,17 @@ gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
  */
 const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
 directionalLight.position.set(0.25, 3, -2.25);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.normalBias = 0.05;
 scene.add(directionalLight);
+
+const directionalLightHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+);
+directionalLightHelper.visible = false;
+scene.add(directionalLightHelper);
 
 /**
  * Debug
@@ -167,6 +179,8 @@ renderer.physicallyCorrectLights = true;
 renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.toneMapping = THREE.ReinhardToneMapping;
 renderer.toneMappingExposure = 1.3;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 gui.add(renderer, "toneMapping", {
   No: THREE.NoToneMapping,
