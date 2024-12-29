7ec501a1f2e79d18a3cfa8cd2a9e45ba56ef43fe
Nati <natibadigojimma@gmail.com>
2024-12-29 02:31:08 -0800

    code refactoring(code restructuring): still refactoring my code with classes and modules

--- Advanced Techniques/code-structuring/src/Utils/Sizes.js
+++ Advanced Techniques/code-structuring/src/Utils/Sizes.js
@@ -1,5 +1,9 @@
-export default class Sizes {
+import EventEmitter from "./EventEmitter";
+
+export default class Sizes extends EventEmitter {
   constructor() {
+    super();
+
     // Setup
     this.width = window.innerWidth;
     this.height = window.innerHeight;
@@ -10,6 +14,7 @@ export default class Sizes {
       this.width = window.innerWidth;
       this.height = window.innerHeight;
       this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+      this.trigger("resize");
     });
   }
 }
