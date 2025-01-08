6f2297f79b47c0995e4d639fc52ca980d3332cc5
Nati <natibadigojimma@gmail.com>
2025-01-08 04:37:00 -0800

    concept(shaders): write my first shader program eventhough i don't know what i wrote

--- Shaders/shaders/src/script.js
+++ Shaders/shaders/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
+import testVertexShader from "./shaders/test/vertex.glsl";
+import testFragmentShader from "./shaders/test/fragment.glsl";
 
 /**
  * Base
@@ -27,26 +29,9 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
 // Material
 const material = new THREE.RawShaderMaterial({
-  vertexShader: `
-    precision mediump float;
-  
-    uniform mat4 projectionMatrix;
-    uniform mat4 viewMatrix;
-    uniform mat4 modelMatrix;
-  
-    attribute vec3 position;
-  
-    void main() {
-      gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    }
-    `,
-  fragmentShader: `
-    precision mediump float;
-  
-    void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
-    }
-    `,
+  vertexShader: testVertexShader,
+  fragmentShader: testFragmentShader,
+  side: THREE.DoubleSide,
 });
 
 // Mesh
