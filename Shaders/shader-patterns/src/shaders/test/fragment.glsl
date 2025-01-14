57f13cbd379e916e5187777d7ce0acde48edff3c
Nati <natibadigojimma@gmail.com>
2025-01-14 01:34:06 -0800

    concept(shaders): trying different shading patterns, am on pattern 32

--- Shaders/shader-patterns/src/shaders/test/fragment.glsl
+++ Shaders/shader-patterns/src/shaders/test/fragment.glsl
@@ -1,7 +1,16 @@
+#define PI 3.14159265359
+
 varying vec2 vUv;
 
 float random (vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid) {
+    return vec2(
+        cos(rotation) * (uv.x - mid.x) - sin(rotation) * (uv.y - mid.y) + mid.x,
+        cos(rotation) * (uv.y - mid.y) + sin(rotation) * (uv.x - mid.x) + mid.y
+    );
 }
 
 void main()
@@ -95,7 +104,27 @@ void main()
     // float strength = length(vUv);
     
     // Pattern 26
-    float strength = length(vUv);
+    // float strength = distance(vUv, vec2(0.5, 0.5));
+    
+    // Pattern 27
+    // float strength = 1.0 - distance(vUv, vec2(0.5, 0.5));
+    
+    // Pattern 28
+    // float strength = 0.15 / distance(vUv, vec2(0.5, 0.5));
+    
+    // Pattern 29
+    // float strength = 0.15 / distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5, 0.5));
+    
+    // Pattern 30
+    // float strength = 0.15 / distance(vec2(vUv.x, (vUv.y - 0.5) * 7.0 + 0.5), vec2(0.5, 0.5));
+    // strength *= 0.15 / distance(vec2((vUv.x - 0.5) * 7.0 + 0.5, vUv.y), vec2(0.5, 0.5));
+    
+    // Pattern 31
+    // float pi = 3.14159265359;
+    
+    vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5, 0.5));
+    float strength = 0.15 / distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 7.0 + 0.5), vec2(0.5, 0.5));
+    strength *= 0.15 / distance(vec2((rotatedUv.x - 0.5) * 7.0 + 0.5, rotatedUv.y), vec2(0.5, 0.5));
     
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
