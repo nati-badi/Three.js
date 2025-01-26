5f6654f428292630d36b8d5cd627d1e6443c1661
Nati <natibadigojimma@gmail.com>
2025-01-26 00:05:31 -0800

    learn(modify shader): corrected the both the core & drop shadow to move with the mesh to look realistic but i gotta say its weird

--- Shaders/modified-materials/src/script.js
+++ Shaders/modified-materials/src/script.js
@@ -71,11 +71,53 @@ const material = new THREE.MeshStandardMaterial({
   normalMap: normalTexture,
 });
 
+const depthMaterial = new THREE.MeshDepthMaterial({
+  depthPacking: THREE.RGBADepthPacking,
+});
+
 const customUniforms = {
   uTime: { value: 0 },
 };
 
 material.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = customUniforms.uTime;
+
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <common>",
+    `
+      #include <common>
+
+      uniform float uTime;
+
+      mat2 get2dRotateMatrix(float _angle) {
+        return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+      }
+    `
+  );
+
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <beginnormal_vertex>",
+    `
+      #include <beginnormal_vertex>
+
+      float angle = (position.y + uTime) * 0.9;
+      mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+      objectNormal.xz = rotateMatrix * objectNormal.xz;
+    `
+  );
+
+  shader.vertexShader = shader.vertexShader.replace(
+    "#include <begin_vertex>",
+    `
+        #include <begin_vertex>
+
+        transformed.xz = rotateMatrix * transformed.xz;
+    `
+  );
+};
+
+depthMaterial.onBeforeCompile = (shader) => {
   shader.uniforms.uTime = customUniforms.uTime;
 
   shader.vertexShader = shader.vertexShader.replace(
@@ -111,6 +153,7 @@ gltfLoader.load("/models/LeePerrySmith/LeePerrySmith.glb", (gltf) => {
   const mesh = gltf.scene.children[0];
   mesh.rotation.y = Math.PI * 0.5;
   mesh.material = material;
+  mesh.customDepthMaterial = depthMaterial; // Update custom depth material
   scene.add(mesh);
 
   // Update materials
