9cbbfd445bb3077aacc9c033c86d0c94763956c3
Nati <natibadigojimma@gmail.com>
2025-02-02 06:43:44 +0300

    exercise(animated galaxy): done with animated galaxy but i don't no nothing.

--- Shaders/animated-galaxy/src/shaders/galaxy/vertex.glsl
+++ Shaders/animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -1,6 +1,10 @@
+uniform float uTime;
 uniform float uSize;
 
 attribute float aScale;
+attribute vec3 aRandomness;
+
+varying vec3 vColor;
 
 
 void main() {
@@ -8,6 +12,19 @@ void main() {
 * Position
 **/
 vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+// Spin
+float angle = atan(modelPosition.x, modelPosition.z);
+float distanceToCenter = length(modelPosition.xz);
+float angleOffset = (1.0 / distanceToCenter) * uTime * 0.2;
+angle += angleOffset;
+modelPosition.x = cos(angle) * distanceToCenter;
+modelPosition.z = sin(angle) * distanceToCenter;
+
+// Randomness
+modelPosition.xyz += aRandomness;
+
+
 vec4 viewPosition = viewMatrix * modelPosition;
 vec4 projectionPosition = projectionMatrix * viewPosition;
 gl_Position = projectionPosition; 
@@ -19,4 +36,10 @@ Star's size
 **/
 gl_PointSize = uSize * aScale;
 gl_PointSize *= (1.0 / - viewPosition.z);
+
+
+/**
+Color
+**/
+vColor = color;
 }
