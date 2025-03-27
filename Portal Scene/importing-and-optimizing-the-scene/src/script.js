f80dbfde27f80dc616a4e8dfc8a5905b3a9136be
Nati <natibadigojimma@gmail.com>
2025-03-27 09:53:47 +0300

    feat(enhancing): added realistic fireflies using shader material

--- Portal Scene/importing-and-optimizing-the-scene/src/script.js
+++ Portal Scene/importing-and-optimizing-the-scene/src/script.js
@@ -3,6 +3,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import firefliesVertexShader from "./shaders/fireflies/vertex.glsl";
+import firefliesFragmentShader from "./shaders/fireflies/fragment.glsl";
 
 /**
  * Base
@@ -78,6 +80,55 @@ gltfLoader.load("portal.glb", (gltf) => {
 });
 
 /**
+ * Fireflies
+ */
+const firefliesGeometry = new THREE.BufferGeometry();
+const firefliesCount = 30;
+const firefliesPositions = new Float32Array(firefliesCount * 3);
+const firefliesScale = new Float32Array(firefliesCount);
+
+for (let i = 0; i < firefliesCount; i++) {
+  firefliesPositions[i * 3 + 0] = (Math.random() - 0.5) * 4;
+  firefliesPositions[i * 3 + 1] = Math.random() * 1.5;
+  firefliesPositions[i * 3 + 2] = (Math.random() - 0.5) * 4;
+
+  firefliesScale[i] = Math.random();
+}
+
+firefliesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(firefliesPositions, 3)
+);
+firefliesGeometry.setAttribute(
+  "aScale",
+  new THREE.BufferAttribute(firefliesScale, 1)
+);
+
+// firefliesMaterial
+const firefliesMaterial = new THREE.ShaderMaterial({
+  vertexShader: firefliesVertexShader,
+  fragmentShader: firefliesFragmentShader,
+  uniforms: {
+    uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+    uSize: { value: 100 },
+  },
+  transparent: true,
+  blending: THREE.AdditiveBlending,
+  depthWrite: false,
+});
+
+gui
+  .add(firefliesMaterial.uniforms.uSize, "value")
+  .min(0)
+  .max(500)
+  .step(1)
+  .name("firefliesSize");
+
+// Points
+const fireflies = new THREE.Points(firefliesGeometry, firefliesMaterial);
+scene.add(fireflies);
+
+/**
  * Sizes
  */
 const sizes = {
@@ -97,6 +148,12 @@ window.addEventListener("resize", () => {
   // Update renderer
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  // Update fireflies
+  firefliesMaterial.uniforms.uPixelRatio.value = Math.min(
+    window.devicePixelRatio,
+    2
+  );
 });
 
 /**
