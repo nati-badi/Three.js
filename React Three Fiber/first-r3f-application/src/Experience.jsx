2397e808c334de49fe9062b7fc0effa7715524e2
Nati <natibadigojimma@gmail.com>
2025-04-03 08:15:20 +0300

    feat(r3f): adding a custom geometry is not just easy as just adding a mesh in r3f

--- React Three Fiber/first-r3f-application/src/Experience.jsx
+++ React Three Fiber/first-r3f-application/src/Experience.jsx
@@ -1,6 +1,7 @@
 import { useFrame, extend, useThree } from "@react-three/fiber";
 import { useRef } from "react";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import CustomObjects from "./CustomObjects";
 
 extend({ OrbitControls });
 
@@ -16,22 +17,27 @@ export default function Experience() {
     <>
       <orbitControls args={[camera, gl.domElement]} />
 
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
       <group ref={cubeRef}>
         <mesh scale={1.5} position-x={2} rotation-y={Math.PI * 0.25}>
           <boxGeometry scale={1.5} />
-          <meshBasicMaterial color="mediumpurple" />
+          <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
         <mesh scale={1.5} position-x={-2} rotation-y={Math.PI * 0.25}>
           <boxGeometry scale={1.5} />
-          <meshBasicMaterial color="orange" />
+          <meshStandardMaterial color="orange" />
         </mesh>
       </group>
 
       <mesh rotation-x={-Math.PI / 2} scale={20} position={[0, -1, 0]}>
         <planeGeometry />
-        <meshBasicMaterial color="green" />
+        <meshStandardMaterial color="green" />
       </mesh>
+
+      <CustomObjects />
     </>
   );
 }
