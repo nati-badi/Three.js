1cad373282ee5370a407e8e192aed9d2a67f0f69
Nati <natibadigojimma@gmail.com>
2025-04-07 08:46:02 +0300

    feat(drei): drei has a lot of features to improve r3f

---
+++ React Three Fiber/r3f-drei/src/Experience.jsx
@@ -0,0 +1,43 @@
+import {
+  OrbitControls,
+  TransformControls,
+  PivotControls,
+} from "@react-three/drei";
+import { useRef } from "react";
+
+export default function Experience() {
+  const cube = useRef();
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        scale={100}
+        fixed={true}
+      >
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </PivotControls>
+
+      <mesh ref={cube} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <TransformControls object={cube} />
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
+}
