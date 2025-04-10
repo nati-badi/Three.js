18e514ce97166888f4f78f7bd4a0d832dfefa459
Nati <natibadigojimma@gmail.com>
2025-04-10 08:02:31 +0300

    feat(debug): trying out leva debug ui

---
+++ React Three Fiber/debug-a-r3f-application/src/Experience.jsx
@@ -0,0 +1,33 @@
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
+
+export default function Experience() {
+  const { position, color } = useControls({
+    position: { value: { x: -2, y: 0 }, step: 0.01, joystick: "invertY" },
+    color: "#ff0000",
+  });
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <mesh position={[position.x, position.y, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+      <mesh position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
+}
